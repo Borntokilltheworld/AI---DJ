2be75be1496b94fbba45951880fa1ef1cb95f8f5
Borntokilltheworld <77736929+Borntokilltheworld@users.noreply.github.com>
2022-01-28 13:38:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+song = ""
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+    
+    posenet = ml5.poseNet(video,ModelLoaded);
+    posenet.on('pose',gotPoses);
+}
+function ModelLoaded(){
+    console.log('Posenet is Initialized');
+}
+function gotPoses(results){
+    if(results.length > 0 ){
+        console.log(results);
+        scoreleftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreleftWrist = "+scoreleftWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = "+leftWristX , "leftWristY = "+leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = "+rightWristX,"rightWristY = "+rightWristY);
+    }
+}
+function draw(){
+    image(video,0,0,600,500);
+    fill("#FF000");
+    stroke("#FF000");
+    if(scoreleftWrist>0.2){
+    circle(leftWristX,leftWristY,20);
+    InNumberleftWristY = Number(leftWristY);
+    remove_decimal = floor(InNumberleftWristY);
+    Innumber_1000_divide = remove_decimal/1000;
+    volume = Innumber_1000_divide * 2;
+    document.getElementById("volume").innerHTML = "Volume = "+volume;
+    song.setVolume(volume);
+    }
+}
+function preload(){
+song = loadSound("music.mp3")
+}
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
